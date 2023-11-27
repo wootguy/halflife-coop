593e690c9151ce3beaa92a01431112f9e7086580
wootguy <drake.rhunter@gmail.com>
2023-11-26 22:01:16 -0800

    fix crash using scientists during a script

--- dlls/talkmonster.cpp
+++ dlls/talkmonster.cpp
@@ -1402,7 +1402,7 @@ BOOL CTalkMonster::CanFollow( void )
 {
 	if ( m_MonsterState == MONSTERSTATE_SCRIPT )
 	{
-		if ( !m_pCine->CanInterrupt() )
+		if (m_pCine && !m_pCine->CanInterrupt() )
 			return FALSE;
 	}
 	
